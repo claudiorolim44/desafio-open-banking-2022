13b9bedb12218406c11e6d15d4d650fbca0729e1
claudiorolim44 <claudioaugusto44@gmail.com>
2022-02-06 21:16:45 -0300

    Criado servidor web com Node e Express rodando na porta 3001

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 // server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Servidor ativo na porta ${PORT}`);
+});
