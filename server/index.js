a688197149805d1d356d42c6f545d454b93b5ebe
claudiorolim44 <claudioaugusto44@gmail.com>
2022-02-06 21:26:28 -0300

    Criado endpoint de API no servidor para futura comunicação com o cliente

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Mensagem de teste da API" });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor ativo na porta ${PORT}`);
 });
