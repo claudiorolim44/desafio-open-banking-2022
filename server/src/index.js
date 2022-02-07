ec668c0dba4a601267b4ab9c0e55f9814182345d
claudiorolim44 <claudioaugusto44@gmail.com>
2022-02-07 01:15:35 -0300

    Adicionada instrução p/ q rotas q não são /api sejam gerenciadas p/React

--- server/src/index.js
+++ server/src/index.js
@@ -12,9 +12,15 @@ const app = express();
 //Fazer o Node servir os arquivos para o cliente React
 app.use(express.static(path.resolve(__dirname, '../../client/build')));
 
+//Gerenciamento das requisições que passam pela rota /api
 app.get('/api', (req, res) => {res.json({ message: "Mensagem teste da API" })});
 app.use('/api/participantes', participantesRouter);
 app.use('/api/servidores-de-autorizacao', servidoresDeAutorizacaoRouter);
+
+//Demais requisições que não passam por /api será gerenciado pelo React
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor ativo na porta ${PORT}`);
