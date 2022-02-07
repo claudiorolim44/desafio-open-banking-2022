b61cf0efcff2ff3ef34e70ebeb888aadb3cf1477
claudiorolim44 <claudioaugusto44@gmail.com>
2022-02-07 09:26:25 -0300

    Adicionado também os logos dos Participantes p/ o Material Ui Data Table

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,28 @@ import React from "react";
 import logo from "./logo.png";
 import MUIDataTable from "mui-datatables";
 
-const colunas = ["Nome", "Url de discovery"];
+const colunas = [
+  {
+    name: "Logo",
+    options: {
+      customBodyRender: (value, tableMeta, updateValue) => (
+        <a href={value}>{value}</a>
+      )
+    }
+  },
+  {
+    name: "Nome"
+  },
+  {
+    name: "Url de discovery",
+    options: {
+      customBodyRender: (value, tableMeta, updateValue) => (
+        <a href={value}>{value}</a>
+      )
+    }
+  },
+];
+
 const opcoes = {};
 
 function App() {
@@ -20,6 +41,7 @@ function App() {
     const dados = [];
     servidoresDeAutorizacao.map(authorisationServer => {
       dados.push([
+        authorisationServer.CustomerFriendlyLogoUri,
         authorisationServer.CustomerFriendlyName,
         authorisationServer.OpenIDDiscoveryDocument
       ]);
