4476e5d01f416f1d759619223184c811c5d5613f
claudiorolim44 <claudioaugusto44@gmail.com>
2022-02-06 22:49:12 -0300

    Criado Model de Participante (Padrão MVC)

---
+++ server/src/models/participante.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const participantesUrl = 
+    'https://data.directory.openbankingbrasil.org.br/participants'
+
+var participanteModelo = {
+    obterTodosParticipantes
+};
+
+// Retorna a lista de participantes fornecidas pelo openbankingbrasil.org.br
+function obterTodosParticipantes(solicitante) {
+    return new Promise((resolve, reject) => {
+        axios.get(participantesUrl)
+        .then((response) => {
+            resolve(response.data);
+        })
+        .catch((error) => {
+            reject(error)
+        });
+    });
+};
+
+
+
+module.exports = participanteModelo;
--- server/src/routes/api/participantes.js
+++ server/src/routes/api/participantes.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const axios = require('axios');
-const participantesUrl = 
-    'https://data.directory.openbankingbrasil.org.br/participants'
+var participante = require('../../models/participante');
 
-// Retorna a lista de participantes fornecidas pelo openbankingbrasil.org.br
+
 router.get('/', (req, res) => {
-    axios.get(participantesUrl)
-    .then((response) => {
-      res.send(response.data);
+    participante.obterTodosParticipantes()
+    .then((data) => {
+      res.send(data);
     })
     .catch((err) => {
         res.status(500).send(err);
