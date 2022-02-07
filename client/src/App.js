b5a18f6a83ceb1b334383b4426af68a5930c5469
claudiorolim44 <claudioaugusto44@gmail.com>
2022-02-07 12:20:33 -0300

    Adicionado texto descritivo na página inicial

--- client/src/App.js
+++ client/src/App.js
@@ -105,6 +105,18 @@ function App() {
               <h1 className="pageTitle">
                 Instituições participantes no Brasil
               </h1>
+              <p className="textoDescritivo">
+                Esta página é a solução de um desafio do desenvolvimento de uma pequena aplicação fullstack que 
+                apresente uma lista de todos os participantes do Open Banking no Brasil (desenvolvido com base em uma API 
+                fornecida pelo <a class="hyperlinks" href={"https://openbankingbrasil.org.br/"}>openbankingbrasil.org.br</a>, 
+                a página oficial do Open Banking no Brasil). A aplicação foi inteiramente desenvolvida via código, 
+                usando HTML, CSS e JavaScript. O front-end foi implementado em React.js e o back-end foi
+                implementado em Node.js, ambos os módulos são desacoplados e se comunicam por APIs internas. O código fonte da aplicação (e todo 
+                o processo de desenvolvimento e commits) pode ser conferido em&nbsp;
+                <a class="hyperlinks" href={"https://github.com/claudiorolim44/desafio-open-banking-2022"}> 
+                 https://github.com/claudiorolim44/desafio-open-banking-2022</a>.
+            </p>              
+            <br/>                  
               <MUIDataTable
                 title={""}
                 data={obterDados()}
